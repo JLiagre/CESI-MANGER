50d5918853689ab69eea278809b490b65c214efb
Lucas Bardon <lucas.bardon@viacesi.fr>
2021-06-24 15:28:47 +0200

    Home Page + gitignore file
    carousel added + css

--- cesi_manger/src/App.vue
+++ cesi_manger/src/App.vue
@@ -6,17 +6,48 @@
           alt="CESI'EAT Logo"
           class="shrink mr-2"
           contain
-          src="@/assets/cesi-manger.jpg"
+          src="@/assets/cesi-manger-logo.png"
           height="60"
           width="60"
         />
       </div>
 
-      <v-spacer></v-spacer>
+      <div class="app-name-bar">
+        <h1>CESI-MANGER</h1>
+      </div>
     </v-app-bar>
 
     <v-main>
       <router-view />
+      <div>
+        <carousel-3d>
+          <slide :index="0">
+              <img style=" width:100%; height: 300px !important;" class="d-block w-100" src="@/assets/carousel1.jpg" alt="first slide">
+          </slide>
+          <slide :index="1">
+              <img style=" width:100%; height: 300px !important;" class="d-block w-100" src="@/assets/carousel2.jpg" alt="second slide">
+          </slide>
+          <slide :index="2">
+              <img style=" width:100%; height: 300px !important;" class="d-block w-100" src="@/assets/carousel3.jpg" alt="third slide">
+          </slide>
+          <slide :index="3">
+              <img style=" width:100%; height: 300px !important;" class="d-block w-100" src="@/assets/carousel4.jpg" alt="fourth slide">
+          </slide>
+          <slide :index="4">
+              <img style=" width:100%; height: 300px !important;" class="d-block w-100" src="@/assets/carousel5.jpg" alt="fifth slide">
+          </slide>
+          <slide :index="5">
+              <img style=" width:100%; height: 300px !important;" class="d-block w-100" src="@/assets/carousel6.jpg" alt="sixth slide">
+          </slide>
+          <slide :index="6">
+              <img style=" width:100%; height: 300px !important;" class="d-block w-100" src="@/assets/carousel7.jpg" alt="seventh slide">
+          </slide>
+          <slide :index="7">
+              <img style=" width:100%; height: 300px !important;" class="d-block w-100" src="@/assets/carousel8.jpg" alt="eighth slide">
+          </slide>
+        </carousel-3d>
+      </div>
+
       <div class="login-button">
         <v-btn size="lg" height="60" width="200">
           <v-icon>mdi-login</v-icon>
@@ -25,7 +56,7 @@
 
         <v-btn size="lg" height="60" width="200">
           <v-icon>mdi-login</v-icon>
-          <span>Restaurent</span>
+          <span>Restaurant</span>
         </v-btn>
     
         <v-btn size="lg" height="60" width="200">
@@ -33,24 +64,53 @@
           <span>Livreur</span>
         </v-btn>
       </div>
+
+      <div class="footer-copyright">  
+        <p> {{copyright}} </p>
+      </div>
     </v-main>
   </v-app>
 </template>
 
 <script>
+import { Carousel3d, Slide } from 'vue-carousel-3d';
+
 export default {
   name: 'App',
 
+  components: {
+    Carousel3d,
+    Slide
+  },
+
   data: () => ({
-    //
+    appName: "CESI-MANGER",
   }),
+
+  computed: {
+			copyright() {
+				const currentYear = new Date().getFullYear()
+
+			return `Copyright ${this.appName} ${currentYear}`
+		}
+	},
 }
 </script>
 
 <style lang="scss">
   .login-button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 15em;
+  };
+  .footer-copyright {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  };
+  .app-name-bar {
     margin-left: auto;
     margin-right: auto;
-    width: 6em;
   };
 </style>
---
+++ cesi_manger/src/views/carouselHomePage.vue
@@ -0,0 +1,24 @@
+<template>
+    <carousel-3d>
+        <slide :index="0">
+            <img class="d-block w-100" src="@/assets/carousel1.jpg" alt="first slide">
+        </slide>
+        <slide :index="1">
+            <img class="d-block w-100" src="@/assets/carousel2.jpg" alt="second slide">
+        </slide>
+        <slide :index="2">
+            <img class="d-block w-100" src="@/assets/carousel3.jpg" alt="third slide">
+        </slide>
+    </carousel-3d>
+</template>
+
+<script>
+import { Carousel3d, Slide } from 'vue-carousel-3d';
+
+export default {
+    components: {
+        Carousel3d,
+        Slide
+    },
+}
+</script>
